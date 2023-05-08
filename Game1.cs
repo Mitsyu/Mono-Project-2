416792b42da84d0a7146ca967be28c20ba88755c
Jila Phonprasith <jilaphonprasith@gmail.com>
2023-05-08 10:14:11 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Mono_Project_2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D blackSquare, whiteSquare;
+      
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 800; 
+            _graphics.PreferredBackBufferHeight = 800; 
+            _graphics.ApplyChanges(); 
+           
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            whiteSquare = Content.Load<Texture2D>("whitesquare");
+            blackSquare = Content.Load<Texture2D>("blacksquare");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            
+            for (int x = 0; x <_graphics.PreferredBackBufferHeight ; x++)
+            {
+                for (int y = 0; y <_graphics.PreferredBackBufferWidth; y++)
+                {
+                    Texture2D squareTexture = ((x + y) % 2 == 0) ? whiteSquare : blackSquare;
+                    _spriteBatch.Draw(squareTexture, new Vector2(x * squareTexture.Width, y * squareTexture.Height), Color.White);
+                }
+            }
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
